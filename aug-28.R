95a442970d5b479741bb3213ea8e83b137a254fa
Kaldrian Moore Jr <kaldrianmoore@yahoo.com>
2024-08-30 09:43:46 -0500

    Add files via upload

---
+++ aug-28.R
@@ -0,0 +1,9 @@
+Print("This is data Science class")
+
+#Author: Kaldrian, August 26 2024, Purpose: Calculate the Correlation Test.
+install.packages("ggpubr")
+Library("ggpubr")
+My_data <-mtcars
+head(My_data)
+
+

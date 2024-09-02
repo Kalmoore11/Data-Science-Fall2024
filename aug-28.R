2b0f78e3b5ace81d7967545a1728ae04f273a940
Kaldrian Moore Jr <kaldrianmoore@yahoo.com>
2024-09-01 20:31:19 -0500

    Delete aug-28.R

--- aug-28.R
+++
@@ -1,9 +0,0 @@
-Print("This is data Science class")
-
-#Author: Kaldrian, August 26 2024, Purpose: Calculate the Correlation Test.
-install.packages("ggpubr")
-Library("ggpubr")
-My_data <-mtcars
-head(My_data)
-
-
